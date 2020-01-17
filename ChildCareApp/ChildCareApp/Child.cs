24d2f2faab27dd8470503d0c3d7d2b06451c051a
Sasya Kethireddy <sasyak@microsoft.com>
2020-01-16 20:46:11 -0800

    Updates with comments

--- ChildCareApp/ChildCareApp/Child.cs
+++ ChildCareApp/ChildCareApp/Child.cs
@@ -6,10 +6,14 @@ namespace ChildCareApp
 {
     public class Child
     {
+        /// <summary>
+        /// This is the child class where you define children details
+        /// </summary>
+        #region
         public string FirstName { get; set; }
         public int Age { get; set; }
         public string LastName { get; set; }
-
+        #endregion
 
     }
 }
--- ChildCareApp/ChildCareApp/Teacher.cs
+++ ChildCareApp/ChildCareApp/Teacher.cs
@@ -6,9 +6,13 @@ namespace ChildCareApp
 {
     public class Teacher
     {
+        /// <summary>
+        /// This class contains details of Teacher
+        /// </summary>
+        #region
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
+        #endregion
 
 
     }
