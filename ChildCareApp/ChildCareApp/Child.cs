28d1e25d6a0e41672159e604cbfae7c32a1c0405
Sasya Kethireddy <sasyak@microsoft.com>
2020-01-16 20:20:10 -0800

    Defining the class

---
+++ ChildCareApp/ChildCareApp/Child.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChildCareApp
+{
+    public class Child
+    {
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+        public string LastName { get; set; }
+
+
+    }
+}
